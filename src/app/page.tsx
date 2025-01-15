9d3b594b792fb46102bf1b2fd8d513ccb3b70f2a
Sander.Eikenes <sander.eikenes2003@gmail.com>
2025-01-15 23:23:33 +0100

    vercel fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/sections/hero";
 import About from "@/components/sections/about";
 import Projects from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
-import Image from "next/image";
 
 export default function Home() {
   return (
