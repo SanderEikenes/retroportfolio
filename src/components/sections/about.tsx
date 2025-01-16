ad2ae98b228f561a137ea48647a490f987a8151e
FIRST_NAME LAST_NAME <sander.eikenes2003@gmail.com>
2025-01-16 12:36:36 +0100

    updates

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -12,7 +12,7 @@ export default function About() {
 
       <div className="md:w-1/3 mx-8 mt-10 md:mt-0">
         <div className="flex space-x-2">
-          <h2 className="font-bold text-6xl">12</h2>
+          <h2 className="font-bold text-6xl">3</h2>
           <p className="font-bold leading-4 translate-y-3.5">
             Completed <br /> projects
           </p>
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex items-center md:justify-between md:mx-40">
-      <div className="flex justify-center items-center mt-20 font-bold mx-auto">
-        <div className="">
-          <h3 className="text-5xl font-semibold w-max">Hi, I'm</h3>
-          <h1 className="text-8xl w-max">
+    <div className="flex justify-between items-center md:mx-40 ">
+      <div className="flex items-center mt-20 font-bold w-full  ">
+        <div className="mx-auto md:mx-0 md:w-full">
+          <h3 className="text-5xl font-semibold w-max md:w-full">Hi, I'm</h3>
+          <h1 className="text-8xl w-max ">
             Sander <br />
             Eikenes
           </h1>
-          <h2 className="w-max text-5xl font-semibold">
+          <h2 className="w-max text-5xl font-semibold md:hidden">
             Web designer & <br />
             Developer
           </h2>
-          <h1 className="font-normal text-xl w-max">
+          <h2 className="w-max text-5xl font-semibold hidden md:block">
+            Web designer & Developer
+          </h2>
+          <h1 className="font-normal text-xl w-max ">
             I'm a web developer from Norway <br /> with several client projects
             completed.
           </h1>
